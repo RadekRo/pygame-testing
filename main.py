2ac7bd9faa3a8440cdc4a8d4ec642408496a7bd2
RadekRo <r.roclawski@ankara.pl>
2023-03-25 18:57:49 +0100

    Player placed on the board

--- main.py
+++ main.py
@@ -24,17 +24,51 @@ def get_background(name):
             tiles.append(pos)
     return tiles, image
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
+
+    player.draw(window)
+    
     pygame.display.update()
 
 class Player(pygame.sprite.Sprite):
-    pass
+    COLOR = (255, 0, 0)
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+    
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("stone-floor-tile.jpg")
+
+    player = Player(100, 100, 50, 50)
 
     run = True
     while run:
@@ -44,7 +78,7 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw(window, background, bg_image)
+        draw(window, background, bg_image, player)
     pygame.quit()
     quit()
 
