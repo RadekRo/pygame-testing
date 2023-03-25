70f25539b308ea7b67779a3122f5ffb1b34c9530
Radek Rocławski <r.roclawski@ankara.pl>
2023-03-25 11:40:03 +0100

    Added /images with stone-floor and wall tiles

---
+++ main.py
@@ -0,0 +1,21 @@
+#import os
+#import random
+#import math
+import pygame
+#from os import listdir
+#from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.quit()
+quit()
+
+if __name__ == "__main__":
+    main(window)
